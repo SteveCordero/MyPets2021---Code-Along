d54c725b21eaf94778ccbc1f068dbd1b2f0d428b
Steve Cordero <stevecords2008@gmail.com>
2021-02-17 19:45:35 +0000

    Introduced polymorphism with two new objects.

--- Main.java
+++ Main.java
@@ -6,15 +6,29 @@ public  class Main
 
     Dog piper = new Dog("Piper", true, "black and white", 10, 0.5, false, 8, "Australian Shepherd");
 
+    PetParent vader = new Dog("Vader", true, "brown", 4, 0.02, false, 1, "sheepdog");
+
     Axolotl matthew = new Axolotl("Matthew", false, "pink", 5, 0.6, true);
 
+    PetParent clifford = new Axolotl("Clifford", false, "purple", 10, 0.01, false);
+
+
+    System.out.println(clifford.dailyRoutine());
     // blankPet.setName("Fluffy");
     // System.out.println("Bob is now called " + blankPet.getName());
-    System.out.println(piper.dailyRoutine());
-    System.out.println(matthew.dailyRoutine());
+    System.out.println(vader.dailyRoutine());
+    System.out.println(vader.dailyRoutine());
+    System.out.println(((Dog)vader).getLoyalty());
 
-    System.out.println("Piper says " + piper.speak());
-    System.out.println("Matthew says " + matthew.speak());
+    PetParent[]myPets = {piper, vader, matthew, clifford};
+
+    for(PetParent animals : myPets)
+    {
+      System.out.println(animals.getName() +  " says " + animals.speak() + ".");
+    }
+
+    // System.out.println("Piper says " + piper.speak());
+    // System.out.println("Matthew says " + matthew.speak());
 
 /*
     System.out.println(blankPet.toString());
